c0ae4e2563a6be7ae02890c7455765ecac996ff7
cemicvetic <cemicvetic@gmail.com>
2025-02-08 15:20:09 -0800

    Add project

--- cucumber.js
+++ cucumber.js
@@ -3,7 +3,11 @@ module.exports = {
         paths: ["src/tests/features"],
         require: ["src/tests/step-definitions/*.ts"],
         requireModule: ["ts-node/register"],
-        format: ["progress-bar", "json:reports/cucumber-report.json", "html:reports/cucumber-report.html"],
+        format: [
+            "progress-bar",
+            "json:reports/cucumber-report.json",
+            "html:reports/cucumber-report.html"
+        ],
         formatOptions: { colorsEnabled: true, snippetInterface: "async-await" }
     }
 };
